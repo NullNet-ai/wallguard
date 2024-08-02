329744a21760ef92f7881862c8911a5d233f1af7
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2024-08-01 23:56:19 -0400

    Implemented simple file monitoring

--- src/utils/file_utils.h
+++ src/utils/file_utils.h
@@ -1,6 +1,8 @@
 #ifndef FILE_UTILS_H
 #define FILE_UTILS_H
 
+#include <stddef.h>
+
 /**
  * @brief Checks if a file exists at the given path.
  *
@@ -16,6 +18,31 @@ int file_exists(const char *path);
  * @param count The number of files in the array.
  * @return 1 if all of the files exist, otherwise 0.
  */
-int files_exist(const char *files[], int count);
+int files_exist(const char *files[], size_t count);
 
-#endif // FILE_UTILS_H
+/**
+ * @brief Struct representing a file monitor.
+ */
+typedef struct {
+    const char *filepath;
+    int         last_update;
+} file_monitor;
+
+/**
+ * @brief Initializes a file monitor.
+ *
+ * @param monitor A pointer to the file_monitor structure to initialize.
+ * @param filepath The path to the file to be monitored.
+ * @return 1 on success, 0 on failure.
+ */
+int file_monitor_init(file_monitor *monitor, const char *filepath);
+
+/**
+ * @brief Checks if the monitored file has been updated.
+ *
+ * @param monitor A pointer to the file_monitor structure.
+ * @return 1 if the file was updated, 0 if not, and -1 on error.
+ */
+int file_monitor_check(file_monitor *monitor);
+
+#endif  // FILE_UTILS_H
