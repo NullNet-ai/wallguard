ce0b03bbbe8bd65686baba0a0ec286fb4467f0e0
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2024-08-08 14:09:39 -0400

    Added inline doc

--- src/main.c
+++ src/main.c
@@ -9,10 +9,11 @@ int main(int argc, char** argv) {
     (void)argc;
     (void)argv;
 
-    revision r;
-    obtain_revision(PLATFORM_OPNSENSE, &r);
-
-    printf("Rev time: %ld\nRev username: %s\n", r.time, r.username);
-
+    revision  r;
+    boolean_t result = obtain_revision(PLATFORM_OPNSENSE, &r);
+    if (result)
+        printf("Rev time: %ld\nRev username: %s\n", r.time, r.username);
+    else
+        printf("Failed to obtain current revision\n");
     return EXIT_SUCCESS;
 }
--- src/platform/revision.h
+++ src/platform/revision.h
@@ -12,6 +12,13 @@ typedef struct {
     time_t time;
 } revision;
 
+/**
+ * @brief Obtains the revision information based on the platform type.
+ *
+ * @param platform The type of platform (PF_SENSE or OPN_SENSE).
+ * @param rev Pointer to the revision structure to store the parsed data.
+ * @return `WM_TRUE` if the revision information was successfully obtained, `WM_FALSE` otherwise.
+ */
 boolean_t obtain_revision(platform_type platform, revision* rev);
 
 #endif
