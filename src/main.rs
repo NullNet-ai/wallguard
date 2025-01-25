030a98ba9d445a13dd0539490bfca2ecfecee5cb
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2025-01-25 04:09:13 +0000

    Setup impl

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod packet_transmitter;
 
 use crate::packet_transmitter::transmitter::transmit_packets;
 use clap::Parser;
-use wallguard_server::WallGuardGrpcInterface;
+use wallguard_server::{Authentication, SetupRequest, WallGuardGrpcInterface};
 
 async fn authenticate(addr: &str, port: u16, app_id: &str, app_secret: &str) -> String {
     WallGuardGrpcInterface::new(addr, port)
@@ -13,6 +13,21 @@ async fn authenticate(addr: &str, port: u16, app_id: &str, app_secret: &str) -> 
         .login(app_id.to_string(), app_secret.to_string())
         .await
         .expect("Authentication failed")
+}
+
+async fn setup(addr: &str, port: u16, token: &str, uuid: &str) {
+    WallGuardGrpcInterface::new(addr, port)
+        .await
+        .setup_client(SetupRequest {
+            auth: Some(Authentication {
+                token: token.to_string(),
+            }),
+            device_version: "2.7-RELEASE".to_string(),
+            device_uuid: uuid.to_string(),
+            hostname: "domain.nullnet.ai".to_string(),
+        })
+        .await
+        .expect("Setup Request Failed");
 }
 
 #[tokio::main]
@@ -28,7 +43,11 @@ async fn main() {
     )
     .await;
 
-    println!("Successful Authentication");
+    println!("Successful Authentication: {:?}", token);
+
+    setup(args.addr.as_str(), args.port, &token, &args.uuid).await;
+
+    println!("Successful Setup");
 
     let mut cfg_watcher =
         confmon_handle::init_confmon(args.addr.clone(), args.port, args.platform.clone()).await;
@@ -45,7 +64,6 @@ async fn main() {
     cfg_monitoring_future.await.unwrap();
 }
 
-
-// @TODO: 
+// @TODO:
 // - Implement token renewal mechanism
 // - Pass token to configuration watcher's callback
