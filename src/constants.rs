69cd7632f745d629f91b53cc5798cb267226521a
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2025-02-07 03:03:39 +0000

    Reduced buffer size to avoid ResourceExhausted Error

--- src/constants.rs
+++ src/constants.rs
@@ -5,12 +5,13 @@ use crate::logger::Logger;
 #[cfg(debug_assertions)]
 pub const BATCH_SIZE: usize = 100;
 #[cfg(not(debug_assertions))]
-pub const BATCH_SIZE: usize = 10_000;
+pub const BATCH_SIZE: usize = 1024; // @Temporary fix to `ResourceExhausted` error
+// pub const BATCH_SIZE: usize = 10_000;
 
 #[cfg(debug_assertions)]
 pub const QUEUE_SIZE: usize = 1_000;
 #[cfg(not(debug_assertions))]
-pub const QUEUE_SIZE: usize = 1_000_000;
+pub const QUEUE_SIZE: usize = 1_000_000; // Isn't it too long ??
 
 pub static UUID: once_cell::sync::Lazy<String> =
     once_cell::sync::Lazy::new(|| uuid::Uuid::new_v4().to_string());
