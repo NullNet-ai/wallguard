d73ddc2789772a25ecd0af17989118ef7e042428
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2025-03-11 02:02:40 +0000

    Replaced warp with actix

---
+++ src/rtty/pty.rs
@@ -0,0 +1,86 @@
+use nullnet_libconfmon::Platform;
+use nullnet_liberror::{location, Error, ErrorHandler, Location};
+use portable_pty::{CommandBuilder, NativePtySystem, PtySize, PtySystem};
+use std::io::{self, Read, Write};
+use std::sync::{Arc, Mutex};
+
+type PtyReader = Box<dyn Read + Send>;
+type PtyWriter = Box<dyn Write + Send>;
+
+pub struct Pty {
+    pub reader: Arc<Mutex<PtyReader>>,
+    pub writer: Arc<Mutex<PtyWriter>>,
+}
+
+impl Pty {
+    pub fn new(platform: &Platform) -> Result<Self, Error> {
+        let pty = NativePtySystem::default()
+            .openpty(PtySize {
+                rows: 24,
+                cols: 80,
+                pixel_height: 0,
+                pixel_width: 0,
+            })
+            .handle_err(location!())?;
+
+        let _ = pty
+            .slave
+            .spawn_command(CommandBuilder::new(pty_program(&platform)))
+            .handle_err(location!())?;
+
+        let reader = pty.master.try_clone_reader().handle_err(location!())?;
+        let writer = pty.master.take_writer().handle_err(location!())?;
+
+        Ok(Self {
+            writer: Arc::new(Mutex::new(writer)),
+            reader: Arc::new(Mutex::new(reader)),
+        })
+    }
+}
+
+impl Write for Pty {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.writer
+            .lock()
+            .map_err(|_| {
+                io::Error::new(
+                    io::ErrorKind::Other,
+                    "Failed to acquire writer lock: write operation aborted",
+                )
+            })?
+            .write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.writer
+            .lock()
+            .map_err(|_| {
+                io::Error::new(
+                    io::ErrorKind::Other,
+                    "Failed to acquire writer lock: flush operation aborted",
+                )
+            })?
+            .flush()
+    }
+}
+
+impl Read for Pty {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        self.reader
+            .lock()
+            .map_err(|_| {
+                io::Error::new(
+                    io::ErrorKind::Other,
+                    "Failed to acquire reader lock: read operation aborted",
+                )
+            })?
+            .read(buf)
+    }
+}
+
+fn pty_program(platform: &Platform) -> &'static str {
+    match platform {
+        Platform::PfSense => "/etc/rc.initial",
+        Platform::OPNsense => "/bin/sh",
+    }
+}
--- src/rtty/session.rs
+++ src/rtty/session.rs
@@ -1,145 +1,73 @@
-use futures_util::{SinkExt, StreamExt};
+use actix::{Actor, AsyncContext, Handler, StreamHandler};
+use actix_web_actors::ws::{self, CloseCode, CloseReason};
 use nullnet_libconfmon::Platform;
-use nullnet_liberror::{location, Error, ErrorHandler, Location};
-use portable_pty::{CommandBuilder, NativePtySystem, PtySize, PtySystem};
-use std::io::Read;
-use std::sync::{Arc, Mutex};
-use tokio::sync::mpsc::{channel, Sender};
-use tokio::task::{self, JoinHandle};
-use uuid::Uuid;
-use warp::ws::WebSocket;
+use nullnet_liberror::Error;
+use std::io::{Read, Write};
+use std::sync::Arc;
+
+use super::pty::Pty;
+use super::pty_message::PtyMessage;
 
 pub struct Session {
-    /// Used to send a `shutdown` signal to the worker.
-    shutdown_signal: Sender<()>,
-    /// The worker's handle.
-    handle: JoinHandle<()>,
-    /// The unique identifier for the session.
-    pub id: uuid::Uuid,
+    pty: Arc<Pty>,
 }
 
 impl Session {
-    /// Spawns a new session that connects a WebSocket to a PTY.
-    pub fn spawn(websocket: WebSocket, platform: Platform, complete_signal: Sender<Uuid>) -> Self {
-        let (shutdown_signal, mut shutdown_receiver) = channel(1);
-
-        let id = Uuid::new_v4();
-
-        let handle = tokio::spawn(async move {
-            let id = id.clone();
-
-            tokio::select! {
-                result = Self::run(websocket, platform) => {
-                    if let Err(err) = result {
-                        log::error!("RemoteTTY session error: {}", err.to_str())
-                    } else {
-                        log::info!("RemoteTTY session completed")
-                    }
-
-                },
-                _ = shutdown_receiver.recv() => log::warn!("RemoteTTY session cancelled")
-            };
-
-            let _ = complete_signal.send(id).await.handle_err(location!());
-        });
-
-        Self {
-            id,
-            handle,
-            shutdown_signal,
-        }
+    pub fn new(platform: Platform) -> Result<Self, Error> {
+        let pty = Pty::new(&platform)?;
+        Ok(Self { pty: Arc::new(pty) })
     }
+}
 
-    /// Gracefully shuts down the session.
-    /// Does NOT send complete signal
-    pub async fn shutdown(self) -> Result<(), Error> {
-        self.shutdown_signal
-            .send(())
-            .await
-            .handle_err(location!())?;
-        self.handle.await.handle_err(location!())
-    }
+impl Actor for Session {
+    type Context = ws::WebsocketContext<Self>;
 
-    /// Runs the session, handling communication between the WebSocket and the PTY.
-    async fn run(websocket: WebSocket, platform: Platform) -> Result<(), Error> {
-        let pty = NativePtySystem::default()
-            .openpty(PtySize {
-                rows: 24,
-                cols: 80,
-                pixel_height: 0,
-                pixel_width: 0,
-            })
-            .handle_err(location!())?;
+    fn started(&mut self, ctx: &mut Self::Context) {
+        let address = ctx.address();
+        let reader = self.pty.reader.clone();
 
-        let _ = pty
-            .slave
-            .spawn_command(CommandBuilder::new(Self::command(platform)))
-            .handle_err(location!())?;
+        tokio::task::spawn_blocking(move || {
+            let mut buffer = [0; 1024];
+            loop {
+                let n = reader
+                    .lock()
+                    .map_or(0, |mut lock| lock.read(&mut buffer).unwrap_or(0));
 
-        let reader = pty.master.try_clone_reader().handle_err(location!())?;
-        let mut writer = pty.master.take_writer().handle_err(location!())?;
-        let (mut ws_tx, mut ws_rx) = websocket.split();
-
-        let (result1, result2) = tokio::join!(
-            // Task to read from WebSocket and write to PTY.
-            task::spawn(async move {
-                while let Some(Ok(msg)) = ws_rx.next().await {
-                    writer.write_all(msg.as_bytes()).handle_err(location!())?;
+                if n == 0 {
+                    break;
                 }
 
-                Ok(())
-            }),
-            // Task to read from PTY and write to WebSocket.
-            tokio::spawn(async move {
-                let reader = Arc::new(Mutex::new(reader));
-                loop {
-                    let read_result =
-                        Self::async_read_msg_from_blocking_reader(reader.clone()).await;
-
-                    if let Ok(message) = read_result {
-                        if message.is_empty() {
-                            break;
-                        }
-
-                        if let Err(_) = ws_tx.send(warp::ws::Message::text(message)).await {
-                            break;
-                        }
-                    } else {
-                        break;
-                    }
-                }
-
-                Ok(())
-            })
-        );
-
-        result1.handle_err(location!())??;
-        result2.handle_err(location!())??;
-
-        Ok(())
-    }
-
-    /// Determines the command to run in the PTY based on the platform.
-    fn command(platform: Platform) -> &'static str {
-        match platform {
-            Platform::PfSense => "/etc/rc.initial",
-            Platform::OPNsense => "/bin/sh",
-        }
-    }
-
-    /// Reads a message from a blocking reader asynchronously.
-    async fn async_read_msg_from_blocking_reader(
-        reader: Arc<Mutex<Box<dyn Read + Send>>>,
-    ) -> Result<String, Error> {
-        tokio::task::spawn_blocking(move || -> Result<String, Error> {
-            let mut buffer = [0u8; 1024];
-            match reader.lock().handle_err(location!())?.read(&mut buffer) {
-                Ok(0) => Ok(String::new()),
-                Ok(n) => Ok(String::from_utf8_lossy(&buffer[..n]).to_string()),
-                Err(err) => Err(err).handle_err(location!()),
+                let pty_message = PtyMessage::from_slice(&buffer[..n]);
+                address.do_send(pty_message);
             }
-        })
-        .await
-        .handle_err(location!())?
+        });
+    }
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Session {
+    fn handle(&mut self, message: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match message {
+            Ok(ws::Message::Text(text)) => {
+                if let Ok(mut lock) = self.pty.writer.lock() {
+                    let _ = lock.write(text.as_bytes());
+                    p
+                } else {
+                    let reason = CloseReason {
+                        code: CloseCode::Error,
+                        description: Some("Failed to write message to pty".to_string()),
+                    };
+                    ctx.close(Some(reason));
+                }
+            }
+            _ => (),
+        }
+    }
+}
+
+impl Handler<PtyMessage> for Session {
+    type Result = ();
+
+    fn handle(&mut self, message: PtyMessage, ctx: &mut Self::Context) -> Self::Result {
+        ctx.text(message);
     }
 }
