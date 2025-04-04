0fc20c6efbaae00e75b36b3b84ec2ac9164d5526
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2025-04-04 00:36:16 +0000

    Implemented single TTY session for all data channels

--- src/rtty/pty.rs
+++ src/rtty/pty.rs
@@ -1,15 +1,23 @@
 use nullnet_libconfmon::Platform;
 use nullnet_liberror::{location, Error, ErrorHandler, Location};
-use portable_pty::{CommandBuilder, NativePtySystem, PtySize, PtySystem};
-use std::io::{self, Read, Write};
-use std::sync::{Arc, Mutex};
+use portable_pty::{CommandBuilder, NativePtySystem, PtyPair, PtySize, PtySystem};
+use std::collections::VecDeque;
+use std::io::{Read, Write};
+use std::sync::{Arc, Mutex as StdMutex};
+use tokio::sync::Mutex as TokioMutex;
+use tokio::sync::{broadcast, mpsc, oneshot};
+
+const MAX_BUFFER_SIZE: usize = 10 * 1024;
+const CHANNEL_CAPACITY: usize = 128;
 
 type PtyReader = Box<dyn Read + Send>;
 type PtyWriter = Box<dyn Write + Send>;
 
 pub struct Pty {
-    pub reader: Arc<Mutex<PtyReader>>,
-    pub writer: Arc<Mutex<PtyWriter>>,
+    buffer: Arc<TokioMutex<VecDeque<u8>>>,
+    to_pty: mpsc::Sender<Vec<u8>>,
+    from_pty: broadcast::Sender<Vec<u8>>,
+    shutdown: oneshot::Sender<()>,
 }
 
 impl Pty {
@@ -28,53 +36,42 @@ impl Pty {
             .spawn_command(CommandBuilder::new(pty_program(platform)))
             .handle_err(location!())?;
 
-        let reader = pty.master.try_clone_reader().handle_err(location!())?;
-        let writer = pty.master.take_writer().handle_err(location!())?;
+        let buffer = Arc::new(TokioMutex::new(VecDeque::new()));
+
+        let (to_pty_tx, to_pty_rx) = mpsc::channel(CHANNEL_CAPACITY);
+        let (from_pty_tx, _) = broadcast::channel(CHANNEL_CAPACITY);
+        let (shutdown_tx, shutdown_rx) = oneshot::channel();
+
+        tokio::spawn(pty_routine(
+            pty,
+            buffer.clone(),
+            to_pty_rx,
+            from_pty_tx.clone(),
+            shutdown_rx,
+        ));
 
         Ok(Self {
-            writer: Arc::new(Mutex::new(writer)),
-            reader: Arc::new(Mutex::new(reader)),
+            buffer,
+            to_pty: to_pty_tx,
+            from_pty: from_pty_tx,
+            shutdown: shutdown_tx,
         })
     }
-}
 
-impl Write for Pty {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.writer
-            .lock()
-            .map_err(|_| {
-                io::Error::new(
-                    io::ErrorKind::Other,
-                    "Failed to acquire writer lock: write operation aborted",
-                )
-            })?
-            .write(buf)
+    pub async fn current_buffer(&self) -> Vec<u8> {
+        self.buffer.lock().await.iter().copied().collect()
     }
 
-    fn flush(&mut self) -> std::io::Result<()> {
-        self.writer
-            .lock()
-            .map_err(|_| {
-                io::Error::new(
-                    io::ErrorKind::Other,
-                    "Failed to acquire writer lock: flush operation aborted",
-                )
-            })?
-            .flush()
+    pub async fn subscribe(&self) -> broadcast::Receiver<Vec<u8>> {
+        self.from_pty.subscribe()
     }
-}
 
-impl Read for Pty {
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        self.reader
-            .lock()
-            .map_err(|_| {
-                io::Error::new(
-                    io::ErrorKind::Other,
-                    "Failed to acquire reader lock: read operation aborted",
-                )
-            })?
-            .read(buf)
+    pub async fn send(&self, message: Vec<u8>) -> Result<(), Error> {
+        self.to_pty.send(message).await.handle_err(location!())
+    }
+
+    pub async fn shutdown(self) {
+        let _ = self.shutdown.send(());
     }
 }
 
@@ -82,5 +79,99 @@ fn pty_program(platform: &Platform) -> &'static str {
     match platform {
         Platform::PfSense => "/etc/rc.initial",
         Platform::OPNsense => "/bin/sh",
+    }
+}
+
+async fn pty_routine(
+    pty_pair: PtyPair,
+    buffer: Arc<TokioMutex<VecDeque<u8>>>,
+    to_pty: mpsc::Receiver<Vec<u8>>,
+    from_pty: broadcast::Sender<Vec<u8>>,
+    shutdown: oneshot::Receiver<()>,
+) -> Result<(), Error> {
+    let reader = pty_pair.master.try_clone_reader().handle_err(location!())?;
+    let writer = pty_pair.master.take_writer().handle_err(location!())?;
+
+    let handle = tokio::spawn(async move {
+        let _ = tokio::join!(
+            reader_routine(reader, buffer, from_pty),
+            writer_routine(writer, to_pty)
+        );
+    });
+
+    tokio::select! {
+        _ = handle => {
+            log::debug!("Pty Session terminated");
+        }
+        _ = shutdown => {
+            log::debug!("Pty Session received shutdown signal");
+        }
+    };
+
+    Ok(())
+}
+
+async fn reader_routine(
+    reader: PtyReader,
+    buffer: Arc<TokioMutex<VecDeque<u8>>>,
+    channel: broadcast::Sender<Vec<u8>>,
+) -> Result<(), Error> {
+    let reader = Arc::new(StdMutex::new(reader));
+
+    loop {
+        let reader = reader.clone();
+
+        let value = tokio::task::spawn_blocking::<_, Result<Vec<u8>, Error>>(move || {
+            let mut buffer = [0; 8192];
+
+            let size: usize = reader
+                .lock()
+                .handle_err(location!())?
+                .read(&mut buffer)
+                .handle_err(location!())?;
+
+            Ok(Vec::from(&buffer[..size]))
+        })
+        .await
+        .handle_err(location!())??;
+
+        if value.is_empty() {
+            break;
+        }
+
+        {
+            let mut lock = buffer.lock().await;
+
+            while lock.len() + value.len() > MAX_BUFFER_SIZE {
+                lock.pop_front();
+            }
+
+            lock.extend(&value);
+        }
+
+        let _ = channel.send(value);
+    }
+
+    Ok(())
+}
+
+async fn writer_routine(
+    writer: PtyWriter,
+    mut channel: mpsc::Receiver<Vec<u8>>,
+) -> Result<(), Error> {
+    let writer = Arc::new(StdMutex::new(writer));
+    loop {
+        let message = channel
+            .recv()
+            .await
+            .ok_or("Channel closed unexpectedly")
+            .handle_err(location!())?;
+
+        let writer = writer.clone();
+
+        tokio::task::spawn_blocking(move || writer.lock().unwrap().write_all(&message))
+            .await
+            .handle_err(location!())?
+            .handle_err(location!())?;
     }
 }
