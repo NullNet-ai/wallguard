5e332f5564ae61e28093496b340c00b3afc15ed8
GyulyVGC <gyulyvgc99@gmail.com>
2025-05-23 16:36:09 +0200

    add TransmissionManager to swithc packet & resource transmission on/off

--- src/data_transmission/mod.rs
+++ src/data_transmission/mod.rs
@@ -1,6 +1,6 @@
 pub(crate) mod dump_dir;
-pub(crate) mod entrypoint;
 pub(crate) mod grpc_handler;
 pub(crate) mod item_buffer;
 pub(crate) mod packets;
 pub(crate) mod resources;
+pub(crate) mod transmission_manager;
--- src/data_transmission/packets/transmitter.rs
+++ src/data_transmission/packets/transmitter.rs
@@ -3,70 +3,63 @@ use crate::constants::{BATCH_SIZE, QUEUE_SIZE};
 use crate::data_transmission::dump_dir::{DumpDir, DumpItem};
 use crate::data_transmission::item_buffer::ItemBuffer;
 use crate::timer::Timer;
+use async_channel::Receiver;
 use nullnet_libwallguard::{Packet, Packets, WallGuardGrpcInterface};
+use nullnet_traffic_monitor::PacketInfo;
 use std::cmp::min;
 use std::sync::Arc;
 use tokio::sync::{Mutex, RwLock};
 
 pub(crate) async fn transmit_packets(
+    rx: Receiver<PacketInfo>,
     args: Args,
     token: Arc<RwLock<String>>,
     dump_dir: DumpDir,
     client: Arc<Mutex<Option<WallGuardGrpcInterface>>>,
 ) {
-    let monitor_config = nullnet_traffic_monitor::MonitorConfig {
-        addr: args.addr.clone(),
-        snaplen: args.snaplen,
-    };
-    let mut rx = nullnet_traffic_monitor::monitor_devices(&monitor_config);
-
     let mut packet_batch = ItemBuffer::new(BATCH_SIZE);
     let mut packet_queue = ItemBuffer::new(QUEUE_SIZE);
     let mut timer = Timer::new(args.transmit_interval);
-    loop {
-        if let Ok(packet) = rx.recv() {
-            let packet = Packet {
-                timestamp: packet.timestamp,
-                interface: packet.interface,
-                link_type: packet.link_type,
-                data: packet.data,
-            };
-            packet_batch.push(packet);
-            if packet_batch.is_full() || timer.is_expired() {
-                timer.reset();
+    while let Ok(packet) = rx.recv().await {
+        let packet = Packet {
+            timestamp: packet.timestamp,
+            interface: packet.interface,
+            link_type: packet.link_type,
+            data: packet.data,
+        };
+        packet_batch.push(packet);
+        if packet_batch.is_full() || timer.is_expired() {
+            timer.reset();
 
-                send_packets(
-                    &client,
-                    &mut packet_batch,
-                    &mut packet_queue,
-                    args.uuid.clone(),
-                    token.read().await.clone(),
-                )
-                .await;
-                if packet_queue.is_full() {
+            send_packets(
+                &client,
+                &mut packet_batch,
+                &mut packet_queue,
+                args.uuid.clone(),
+                token.read().await.clone(),
+            )
+            .await;
+            if packet_queue.is_full() {
+                log::warn!(
+                    "Queue is full. Dumping {} packets to file",
+                    packet_queue.len(),
+                );
+                let dump_item = DumpItem::Packets(Packets {
+                    uuid: args.uuid.clone(),
+                    packets: packet_queue.take(),
+                    token: String::new(),
+                });
+                dump_dir.dump_item_to_file(dump_item).await;
+                if dump_dir.is_full().await {
                     log::warn!(
-                        "Queue is full. Dumping {} packets to file",
-                        packet_queue.len(),
+                        "Dump size maximum limit reached. Packets routine entering idle mode...",
                     );
-                    let dump_item = DumpItem::Packets(Packets {
-                        uuid: args.uuid.clone(),
-                        packets: packet_queue.take(),
-                        token: String::new(),
-                    });
-                    dump_dir.dump_item_to_file(dump_item).await;
-                    if dump_dir.is_full().await {
-                        log::warn!("Dump size maximum limit reached. Packets routine entering idle mode...",);
-                        // stop traffic monitoring
-                        drop(rx);
-                        // wait for the server to come up again
-                        loop {
-                            tokio::time::sleep(std::time::Duration::from_secs(10)).await;
-                            if client.lock().await.is_some() {
-                                break;
-                            }
+                    // wait for the server to come up again
+                    loop {
+                        tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+                        if client.lock().await.is_some() {
+                            break;
                         }
-                        // restart traffic monitoring
-                        rx = nullnet_traffic_monitor::monitor_devices(&monitor_config);
                     }
                 }
             }
