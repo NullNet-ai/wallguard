1a2161554f1a80638968263357958e2fef405b29
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2025-05-28 16:44:00 -0400

    System UUID (#9)

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use control_channel::ControlChannel;
 mod app_context;
 mod cli;
 mod control_channel;
+mod device_uuid;
 mod pty;
 mod reverse_tunnel;
 mod token_provider;
@@ -12,6 +13,19 @@ mod utilities;
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let context = AppContext::new().await;
-    ControlChannel::new(context).run().await.unwrap();
+
+    if !nix::unistd::Uid::effective().is_root() {
+        log::error!("This program must be run as root. Exiting ...");
+        std::process::exit(-1);
+    }
+
+    let Some(device_uuid) = device_uuid::retrieve_device_uuid() else {
+        log::error!("Failed to retrieve device UUID, exiting ...");
+        std::process::exit(-1);
+    };
+
+    println!("Device UUID: {}", device_uuid);
+
+    // let context = AppContext::new().await;
+    // ControlChannel::new(context).run().await.unwrap();
 }
