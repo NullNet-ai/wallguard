139e331718f6ba7c03f4888291d20f864d03f311
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2025-05-28 20:39:26 +0000

    Removed all parameters except for system uuid

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use app_context::AppContext;
 use control_channel::ControlChannel;
-use devfingerprint::devfingerprint;
 
 mod app_context;
 mod cli;
 mod control_channel;
-mod devfingerprint;
+mod device_uuid;
 mod pty;
 mod reverse_tunnel;
 mod token_provider;
@@ -15,12 +14,17 @@ mod utilities;
 async fn main() {
     env_logger::init();
 
-    let Some(devfingerprint) = devfingerprint() else {
-        log::error!("Failed to calculate device fingerprint, exiting ...");
+    if !nix::unistd::Uid::effective().is_root() {
+        log::error!("This program must be run as root. Exiting ...");
+        std::process::exit(-1);
+    }
+
+    let Some(device_uuid) = device_uuid::retrieve_device_uuid() else {
+        log::error!("Failed to retrieve device UUID, exiting ...");
         std::process::exit(-1);
     };
 
-    println!("Device fingerprint: {}", devfingerprint);
+    println!("Device UUID: {}", device_uuid);
 
     // let context = AppContext::new().await;
     // ControlChannel::new(context).run().await.unwrap();
