b5b6a6b9f7e2db8169ce51ec55b5202eaaa71997
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2025-05-28 23:27:09 +0000

    Defined proto and generated code

---
+++ wallguard/build.rs
@@ -0,0 +1,8 @@
+pub fn main() {
+    tonic_build::configure()
+        .build_client(false)
+        .build_server(true)
+        .out_dir("./src/cli")
+        .compile_protos(&["../proto/cli.proto"], &["../proto"])
+        .expect("Failed to compile CLI proto file.")
+}
