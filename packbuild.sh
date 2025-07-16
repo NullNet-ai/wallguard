c987378064967aeb50bbd510d770ff1ff95930fb
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2025-07-16 03:59:30 +0000

    Added `freebsd` package to `packbuild.sh`

--- packbuild.sh
+++ packbuild.sh
@@ -11,9 +11,20 @@ case "$1" in
 
         dpkg-deb --build packages/debian ./
 
-        rm packages/debian/usr/local/bin/*
-
+        rm packages/debian/usr/local/bin/wallguard
+        rm packages/debian/usr/local/bin/wallguard-cli
         ;;
+    freebsd)
+        cargo build --release -p wallguard -p wallguard-cli
+
+        mkdir -p packages/freebsd/usr/local/bin
+        cp target/release/wallguard packages/freebsd/usr/local/bin/
+        cp target/release/wallguard-cli packages/freebsd/usr/local/bin/
+
+        pkg create -M packages/freebsd/+MANIFEST -r packages/freebsd
+
+        rm packages/freebsd/usr/local/bin/wallguard
+        rm packages/freebsd/usr/local/bin/wallguard-cli
     *)
         echo "Unsupported or missing parameter."
         exit 1
