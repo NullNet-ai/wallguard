2e6362b19f2ac294fee02ebc5aaf7709d674c793
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2026-02-02 21:14:09 -0500

    Fixed clippy warnings

--- wallguard/src/control_channel/commands/open_remote_desktop_session_command.rs
+++ wallguard/src/control_channel/commands/open_remote_desktop_session_command.rs
@@ -1,6 +1,9 @@
-use crate::context::Context;
 use crate::control_channel::command::ExecutableCommand;
-use nullnet_liberror::Error;
+use crate::remote_desktop::RemoteDesktopManager;
+use crate::{context::Context, reverse_tunnel::TunnelInstance};
+use nullnet_liberror::{Error, ErrorHandler, Location, location};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, ReadHalf, WriteHalf};
+use tokio::sync::mpsc;
 
 pub struct OpenRemoteDesktopSessionCommand {
     context: Context,
@@ -17,88 +20,75 @@ impl ExecutableCommand for OpenRemoteDesktopSessionCommand {
     async fn execute(self) -> Result<(), Error> {
         log::debug!("Received OpenRemoteDesktopSessionCommand");
 
-        // if !self
-        //     .context
-        //     .client_data
-        //     .platform
-        //     .can_open_remote_desktop_session()
-        // {
-        //     return Err("Cannot open remote desktop session: unsupported session")
-        //         .handle_err(location!());
-        // }
+        if !self
+            .context
+            .client_data
+            .platform
+            .can_open_remote_desktop_session()
+        {
+            return Err("Cannot open remote desktop session: unsupported session")
+                .handle_err(location!());
+        }
 
-        // let Ok(tunnel) = self.context.tunnel.request_channel(&self.token).await else {
-        //     return Err("Cant establish tunnel connection").handle_err(location!());
-        // };
+        let Ok(tunnel) = self.context.tunnel.request_channel(&self.token).await else {
+            return Err("Cant establish tunnel connection").handle_err(location!());
+        };
 
-        // let Some(mut rdm) = self.context.remote_desktop_manager.clone() else {
-        //     return Err("Remote Desktop is not available").handle_err(location!());
-        // };
+        let Some(mut rdm) = self.context.remote_desktop_manager.clone() else {
+            return Err("Remote Desktop is not available").handle_err(location!());
+        };
 
-        // tokio::spawn(async move {
-        //     let (sender, receiver) = mpsc::channel(64);
-        //     let id = rdm.on_client_connected(sender).await;
+        tokio::spawn(async move {
+            let (sender, receiver) = mpsc::channel(64);
+            let id = rdm.on_client_connected(sender).await;
 
-        //     tokio::select! {
-        //         _ = stream_to_system(tunnel.reader, rdm.clone(), id) => {},
-        //         _ = system_to_stream(tunnel.writer, receiver) => {},
-        //     }
+            let (reader, writer) = tokio::io::split(tunnel);
 
-        //     let _ = rdm.on_client_disconnected(id).await;
-        // });
+            tokio::select! {
+                _ = stream_to_system(reader, rdm.clone(), id) => {},
+                _ = system_to_stream(writer, receiver) => {},
+            }
+
+            let _ = rdm.on_client_disconnected(id).await;
+        });
 
         Ok(())
     }
 }
 
-// async fn stream_to_system(
-//     reader: TunnelReader,
-//     remote_desktop_manager: RemoteDesktopManager,
-//     client_id: u128,
-// ) -> Result<(), Error> {
-//     loop {
-//         let message = reader
-//             .lock()
-//             .await
-//             .message()
-//             .await
-//             .handle_err(location!())?
-//             .ok_or("End of stream")
-//             .handle_err(location!())?
-//             .message
-//             .ok_or("Unexpected empty message")
-//             .handle_err(location!())?;
+async fn stream_to_system(
+    mut reader: ReadHalf<TunnelInstance>,
+    remote_desktop_manager: RemoteDesktopManager,
+    client_id: u128,
+) -> Result<(), Error> {
+    loop {
+        let mut buffer = [0; 4096];
+        let bytes = reader.read(&mut buffer).await.handle_err(location!())?;
 
-//         let ServerMessage::Data(data_frame) = message else {
-//             return Err("Unexpected message type").handle_err(location!())?;
-//         };
+        let message = buffer[..bytes].to_vec();
 
-//         if let Err(err) = remote_desktop_manager
-//             .on_client_message(client_id, data_frame.data)
-//             .await
-//         {
-//             log::error!(
-//                 "OpenRemoteDesktopSessionCommand: Failed to handle client message: {}",
-//                 err.to_str()
-//             );
-//         }
-//     }
-// }
+        if let Err(err) = remote_desktop_manager
+            .on_client_message(client_id, message)
+            .await
+        {
+            log::error!(
+                "OpenRemoteDesktopSessionCommand: Failed to handle client message: {}",
+                err.to_str()
+            );
+        }
+    }
+}
 
-// async fn system_to_stream(
-//     writer: TunnelWriter,
-//     mut receiver: mpsc::Receiver<Vec<u8>>,
-// ) -> Result<(), Error> {
-//     while let Some(data) = receiver.recv().await {
-//         writer
-//             .lock()
-//             .await
-//             .send(ClientFrame {
-//                 message: Some(ClientMessage::Data(DataFrame { data })),
-//             })
-//             .await
-//             .handle_err(location!())?;
-//     }
+async fn system_to_stream(
+    mut writer: WriteHalf<TunnelInstance>,
+    mut receiver: mpsc::Receiver<Vec<u8>>,
+) -> Result<(), Error> {
+    while let Some(data) = receiver.recv().await {
+        writer
+            .write_all(data.as_slice())
+            .await
+            .handle_err(location!())?;
+    }
 
-//     Ok(())
-// }
+    Ok(())
+}
--- wallguard/src/control_channel/commands/open_tty_session_command.rs
+++ wallguard/src/control_channel/commands/open_tty_session_command.rs
@@ -1,5 +1,12 @@
+use std::sync::Arc;
+
+use nullnet_liberror::{Error, ErrorHandler, Location, location};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, ReadHalf, WriteHalf};
+
 use crate::context::Context;
 use crate::control_channel::command::ExecutableCommand;
+use crate::pty::{Pty, PtyReader, PtyWriter};
+use crate::reverse_tunnel::TunnelInstance;
 
 pub struct OpenTtySessionCommand {
     context: Context,
@@ -16,72 +23,65 @@ impl ExecutableCommand for OpenTtySessionCommand {
     async fn execute(self) -> Result<(), nullnet_liberror::Error> {
         log::debug!("Received OpenTtySessionCommand");
 
-        // let pty = Pty::new("/bin/sh")?;
+        let pty = Pty::new("/bin/sh")?;
 
-        // let Ok(tunnel) = self.context.tunnel.request_channel(&self.token).await else {
-        //     return Err("Cant establish tunnel connection").handle_err(location!());
-        // };
+        let Ok(tunnel) = self.context.tunnel.request_channel(&self.token).await else {
+            return Err("Cant establish tunnel connection").handle_err(location!());
+        };
 
-        // tokio::spawn(async move {
-        //     tokio::select! {
-        //         _ = stream_to_pty(tunnel.reader, pty.writer) => {},
-        //         _ = pty_to_stream(tunnel.writer, pty.reader) => {},
-        //     }
-        // });
+        tokio::spawn(async move {
+            let (reader, writer) = tokio::io::split(tunnel);
+
+            tokio::select! {
+                _ = stream_to_pty(reader, pty.writer) => {},
+                _ = pty_to_stream(writer, pty.reader) => {},
+            }
+        });
 
         Ok(())
     }
 }
 
-// async fn stream_to_pty(reader: TunnelReader, writer: PtyWriter) -> Result<(), Error> {
-//     loop {
-//         let message = reader
-//             .lock()
-//             .await
-//             .message()
-//             .await
-//             .handle_err(location!())?
-//             .ok_or("End of stream")
-//             .handle_err(location!())?
-//             .message
-//             .ok_or("Unexpected empty message")
-//             .handle_err(location!())?;
+async fn stream_to_pty(
+    mut reader: ReadHalf<TunnelInstance>,
+    writer: PtyWriter,
+) -> Result<(), Error> {
+    loop {
+        let mut buffer = [0; 4096];
+        let bytes = reader.read(&mut buffer).await.handle_err(location!())?;
 
-//         let ServerMessage::Data(data_frame) = message else {
-//             return Err("Unexpected message type").handle_err(location!())?;
-//         };
+        let message = buffer[..bytes].to_vec();
 
-//         let writer = Arc::clone(&writer);
-//         tokio::task::spawn_blocking(move || writer.lock().unwrap().write_all(&data_frame.data))
-//             .await
-//             .handle_err(location!())?
-//             .handle_err(location!())?;
-//     }
-// }
+        let writer = Arc::clone(&writer);
+        tokio::task::spawn_blocking(move || writer.lock().unwrap().write_all(&message))
+            .await
+            .handle_err(location!())?
+            .handle_err(location!())?;
+    }
+}
 
-// async fn pty_to_stream(writer: TunnelWriter, reader: PtyReader) -> Result<(), Error> {
-//     loop {
-//         let reader = Arc::clone(&reader);
+async fn pty_to_stream(
+    mut writer: WriteHalf<TunnelInstance>,
+    reader: PtyReader,
+) -> Result<(), Error> {
+    loop {
+        let reader = Arc::clone(&reader);
 
-//         let data = tokio::task::spawn_blocking(move || {
-//             let mut buf = [0u8; 8196];
-//             match reader.lock().unwrap().read(&mut buf) {
-//                 Ok(0) => Ok(Vec::new()), // EOF
-//                 Ok(n) => Ok(buf[..n].to_vec()),
-//                 Err(err) => Err(err),
-//             }
-//         })
-//         .await
-//         .handle_err(location!())?
-//         .handle_err(location!())?;
+        let data = tokio::task::spawn_blocking(move || {
+            let mut buf = [0u8; 8196];
+            match reader.lock().unwrap().read(&mut buf) {
+                Ok(0) => Ok(Vec::new()), // EOF
+                Ok(n) => Ok(buf[..n].to_vec()),
+                Err(err) => Err(err),
+            }
+        })
+        .await
+        .handle_err(location!())?
+        .handle_err(location!())?;
 
-//         writer
-//             .lock()
-//             .await
-//             .send(ClientFrame {
-//                 message: Some(ClientMessage::Data(DataFrame { data })),
-//             })
-//             .await
-//             .handle_err(location!())?;
-//     }
-// }
+        writer
+            .write_all(data.as_slice())
+            .await
+            .handle_err(location!())?;
+    }
+}
